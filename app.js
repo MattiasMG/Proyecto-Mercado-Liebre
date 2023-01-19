96e3d2ab011a73cb8bd2cd2ce26b76dac2a09c2f
MattiasMG <matias.munozgader@gmail.com>
2023-01-18 22:31:47 -0300

    Render

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
 const port = process.env.PORT || 3001;
-app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
+app.listen (port, () => console.log('Servidor corriendo en el puerto ${port}'));
 
 app.get ('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
